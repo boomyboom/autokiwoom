63aaa9d8871b7260356a59f5633e97d306f4b757
boomyboom <44042708+boomyboom@users.noreply.github.com>
2021-07-11 13:47:59 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name              = "autokiwoom',
+  version           = '1.0.0.0',
+  description       = 'Python wrapper for Kiwoom Open API+ (Korea Stock Market)',
+  author            = 'Seongbeom Park',
+  author_email      = 'boomybooom@gmail.com',
+  url               = 'https://github.com/boomyboom/autokiwoom',
+  install_requires  = ['pandas']
+  license           = 'MIT'
+  packages          = ['autokiwoom']
+  zip_safe          = False
+)
