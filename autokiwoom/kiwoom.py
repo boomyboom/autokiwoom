e9638b125d8472c52d4165db050c1dddd299371d
boomyboom <44042708+boomyboom@users.noreply.github.com>
2021-08-16 19:28:04 +0900

    Update kiwoom.py

--- autokiwoom/kiwoom.py
+++ autokiwoom/kiwoom.py
@@ -1,2 +1,26 @@
-import sys
-from PyQt5.QtWidgets import *
+from PyQt5.QAxContainer import *
+from autokiwoom.omsg import outputmsg
+
+class Kiwoom():
+    def __init__(self):
+        super().__init__()
+        print("키움 클래스입니다.")
+        self.ocx = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.CommConnect()
+        self.set_signal_slot()
+
+        #self.handle_login()
+
+    def handler_msg(self, err_code):
+        if err_code == 0:
+            print("로그인 성공", outputmsg(err_code)[1])
+        else:
+            print("로그인 실패", outputmsg(err_code)[1])
+
+    # signal
+    def set_signal_slot(self):
+        self.ocx.OnEventConnect.connect(self.handler_msg)
+
+    #method
+    def CommConnect(self, block=True):
+        self.ocx.dynamicCall("CommConnect()")
